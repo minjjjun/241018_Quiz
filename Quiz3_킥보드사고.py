6f999c060746b4e400c52e78b00d5745719b7727
minjjjun <163504482+minjjjun@users.noreply.github.com>
2024-10-22 00:03:44 +0900

    Update Quiz3_킥보드사고.py

--- Quiz3_킥보드사고.py
+++ Quiz3_킥보드사고.py
@@ -3,29 +3,37 @@
 
 class ScooterAccidentDisplaySystem:
     def __init__(self):
+        
         # 도로명과 사고 딕셔너리
         self.accident_records = {}
 
     # 사고 발생 기록 추가(road_name:도로명, cause:사고원인)
     def record_accident(self, road_name, cause):
+        
         # 도로명이 accident_records에 없으면 새로운 리스트 생성
         if road_name not in self.accident_records:
             self.accident_records[road_name] = []
+        
         # 사고원인을 리스트에 추가
         self.accident_records[road_name].append(cause)
+        
         # 사고 발생 도로, 원인 출력
         print(f"{road_name}에서 사고가 기록되었습니다: 원인 - {cause}")
 
     # 도로별 사고 현황 표지판 형식으로 표시
     def display_accident_status(self, road_name):
-         # 도로명이 accident_records에 존재하면
+         
+        # 도로명이 accident_records에 존재하면
         if road_name in self.accident_records:
             accidents = self.accident_records[road_name]
+           
             # 그 도로의 사고 수를 세어 나타냄
             print(f"[표지판] {road_name}에서 발생한 사고: {len(accidents)}건")
+            
             # 그 사고의 원인
             for cause in accidents:
                 print(f"- 원인: {cause}")
+        
         # 도로명이 accident_records에 존재하지 않을 경우
         else:
             print(f"{road_name}에서는 사고가 기록되지 않았습니다.")
